da037006e30679fe65469d1237f0963177099409
KutmanBekbolotov <kutmanbekbolotov2003@gmail.com>
2024-10-25 20:19:38 +0600

    added api key

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Hello")
+import requests
+from PIL import Image
+from io import BytesIO
+
+WEATHER_API_KEY = '9c9b46711ffc5930547a7e8a82bd084b'
+IMAGE_API_KEY = 'ptOiEf2rqn144-_oYNmP78mEj9Ji1hENqoKc_sekQS0'
