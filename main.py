00eab02368ceb3b43df198c955cfe6bf75d2557f
KutmanBekbolotov <kutmanbekbolotov2003@gmail.com>
2024-10-25 20:22:40 +0600

    added function

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ from io import BytesIO
 
 WEATHER_API_KEY = '9c9b46711ffc5930547a7e8a82bd084b'
 IMAGE_API_KEY = 'ptOiEf2rqn144-_oYNmP78mEj9Ji1hENqoKc_sekQS0'
+
+def get_weather(city):
+
+    weather_url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={WEATHER_API_KEY}&units=metric"
+    
